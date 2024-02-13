0f5863f9c234626d41d8cce61d3bc2f6cb397731
YuBin <117196984+lcsine@users.noreply.github.com>
2024-02-13 15:21:07 +0800

    domain对象

--- tangchat-chat-server/src/main/java/com/tang/tangchat/common/websocket/domain/vo/resp/WSMsgMark.java
+++ tangchat-chat-server/src/main/java/com/tang/tangchat/common/websocket/domain/vo/resp/WSMsgMark.java
@@ -27,14 +27,14 @@ public class WSMsgMark {
         @ApiModelProperty("消息id")
         private Long msgId;
         /**
-         * @see com.abin.mallchat.common.chat.domain.enums.MessageMarkTypeEnum
+//         * @see com.tang.tangchat.common.websocket.domain.enums.MessageMarkTypeEnum
          */
         @ApiModelProperty("标记类型 1点赞 2举报")
         private Integer markType;
         @ApiModelProperty("被标记的数量")
         private Integer markCount;
         /**
-         * @see com.abin.mallchat.common.chat.domain.enums.MessageMarkActTypeEnum
+//         * @see com.tang.tangchat.common.websocket.domain.enums.MessageMarkActTypeEnum
          */
         @ApiModelProperty("动作类型 1确认 2取消")
         private Integer actType;
